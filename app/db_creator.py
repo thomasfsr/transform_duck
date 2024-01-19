0926906e6d23e01d787c1d4166f12f4da965f92d
Thomás Freire <thomas.fsr@gmail.com>
2024-01-19 10:53:02 -0300

    implementação da criação do diretório output

--- app/db_creator.py
+++ app/db_creator.py
@@ -1,27 +1,33 @@
 import duckdb
+import os
 
 class DuckdbCreation:
-    def __init__(self, name_db:str='database', path:str='data/arquivos_csv'):
+    def __init__(self, name_db:str='database', input_path:str='data/arquivos_csv'):
         self.name_db = name_db
-        self.path = path
+        self.input_path = input_path
         self.parquet_file = f"df_{self.name_db}"
-    
+        self.output_dir = 'output'
+
     def start(self):
-        conn = self.create_db_conn()
+        self.create_output_directory()
+        conn = self.connect_to_db()
         self.merge_csv_files(conn)
         self.save_as_parquet(conn)
-        self.close_connection(conn) 
+        self.close_connection(conn)
+
+    def create_output_directory(self):
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
           
-    def create_db_conn(self):
-        conn = duckdb.connect(database=f'{self.name_db}.db')
+    def connect_to_db(self):
+        conn = duckdb.connect(database=f'{self.output_dir}/{self.name_db}.db')
         return conn
         
     def merge_csv_files(self, conn):
-        conn.execute(f"CREATE TABLE transactions AS SELECT * FROM read_csv_auto('{self.path}/*.csv', filename=true)")
+        conn.execute(f"CREATE TABLE transactions AS SELECT * FROM read_csv_auto('{self.input_path}/*.csv', filename=true)")
 
-    def save_as_parquet(self, conn):
-        #conn.execute(f"COPY transactions TO "{self.parquet_file}.parquet" ")
-        conn.execute(f"COPY transactions TO '{self.parquet_file}.parquet' (FORMAT PARQUET)")
+    def save_as_parquet(self, conn):        
+        conn.execute(f"COPY transactions TO '{self.output_dir}/{self.parquet_file}.parquet' (FORMAT PARQUET)")
     
     def close_connection(self, conn):
         conn.close()
--- start.py
+++ start.py
@@ -3,7 +3,7 @@ from app.db_creator import DuckdbCreation
 dbname = 'transaction'
 path = 'data/arquivos_csv'
 # Instantiate the class
-duckdb_creator = DuckdbCreation(name_db= dbname, path= path)
+duckdb_creator = DuckdbCreation(name_db= dbname, input_path= path)
 
 # Call the start method to begin the process
 duckdb_creator.start()
