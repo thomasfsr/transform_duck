feee901f5dbf63f1ebea64c2acfdcd2a883c31f4
Thomás Freire <thomas.fsr@gmail.com>
2024-01-21 14:48:46 -0300

    melhora dos argumentos passados para a classe

--- app/db_tool.py
+++ app/db_tool.py
@@ -2,18 +2,18 @@ import duckdb
 import os
 
 class DuckdbTransform:
-    def __init__(self, name_db:str='database', name_tbl: str = 'transactions', input_path:str='data/arquivos_csv'):
-        self.name_db = name_db
+    def __init__(self, db_name:str='database', tbl_name: str = 'table', input_path:str='data/arquivos_csv'):
+        self.db_name = db_name
         self.input_path = input_path
-        self.parquet_file = f"df_{self.name_db}"
+        self.parquet_file = f"df_{self.db_name}"
         self.output_dir = 'database'
-        self.name_tbl = name_tbl
+        self.tbl_name = tbl_name
 
     def start(self):
         self.create_output_directory()
         conn = self.connect_to_db()
         self.merge_csv_files(conn)
-        self.validation(conn)
+        self.cleaner(conn)
         self.save_as_parquet(conn)
         self.close_connection(conn)
 
@@ -22,19 +22,19 @@ class DuckdbTransform:
             os.makedirs(self.output_dir)
           
     def connect_to_db(self):
-        conn = duckdb.connect(database=f'{self.output_dir}/{self.name_db}.db')
+        conn = duckdb.connect(database=f'{self.output_dir}/{self.db_name}.db')
         return conn
         
     def merge_csv_files(self, conn):
-        conn.execute(f"CREATE TABLE {self.name_tbl} AS SELECT * FROM read_csv_auto('{self.input_path}/*.csv', filename=true)")
+        conn.execute(f"CREATE TABLE {self.tbl_name} AS SELECT * FROM read_csv_auto('{self.input_path}/*.csv', filename=true)")
 
     def save_as_parquet(self, conn):        
-        conn.execute(f"COPY cleansed_{self.name_tbl} TO '{self.output_dir}/{self.parquet_file}.parquet' (FORMAT PARQUET)")
+        conn.execute(f"COPY cleansed_{self.tbl_name} TO '{self.output_dir}/{self.parquet_file}.parquet' (FORMAT PARQUET)")
     
     def close_connection(self, conn):
         conn.close()
     
-    def validation(self, conn):
-        validation_query = f'''CREATE TABLE cleansed_{self.name_tbl} AS SELECT * FROM {self.name_tbl} WHERE price > 0 '''
-        conn.execute(validation_query)
+    def cleaner(self, conn):
+        cleaner_query = f'''CREATE TABLE cleansed_{self.tbl_name} AS SELECT * FROM {self.tbl_name} WHERE price > 0 '''
+        conn.execute(cleaner_query)
 
--- start.py
+++ start.py
@@ -2,8 +2,9 @@ from app.db_tool import DuckdbTransform
 
 dbname = 'transaction'
 path = 'data/arquivos_csv'
+tablename = 'transactions'
 # Instantiate the class
-duckdb_creator = DuckdbTransform(name_db= dbname, input_path= path)
+duckdb_creator = DuckdbTransform(db_name = dbname, input_path= path, table_name = tablename)
 
 # Call the start method to begin the process
 duckdb_creator.start()
