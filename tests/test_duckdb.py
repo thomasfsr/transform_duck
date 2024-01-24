7079b8797b337a7543608420263d192d0d40d712
Thomás Freire <thomas.fsr@gmail.com>
2024-01-24 12:57:58 -0300

    atualizando o teste com a nova classe

--- tests/test_duckdb.py
+++ tests/test_duckdb.py
@@ -1,12 +1,12 @@
-from app.db_tool import DuckdbTransform
+from app.db_tools import DuckdbClass
 import os
 
 def test_create_output_directory():
-    duckdb_creator = DuckdbTransform()
+    duckdb_creator = DuckdbClass()
     duckdb_creator.create_output_directory()
     assert os.path.exists(duckdb_creator.output_dir)
 
 def test_connect_to_db():
-    duckdb_creator = DuckdbTransform()
+    duckdb_creator = DuckdbClass()
     conn = duckdb_creator.connect_to_db()
     assert conn is not None
