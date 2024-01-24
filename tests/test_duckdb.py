f967f243bf0167feaf1c89ed4446ec961c2c2c16
Thomás Freire <thomas.fsr@gmail.com>
2024-01-24 13:05:21 -0300

    Atualizando o teste e dash

--- tests/test_duckdb.py
+++ tests/test_duckdb.py
@@ -3,10 +3,10 @@ import os
 
 def test_create_output_directory():
     duckdb_creator = DuckdbClass()
-    duckdb_creator.create_output_directory()
+    duckdb_creator.create_output_directory('output_teste')
     assert os.path.exists(duckdb_creator.output_dir)
 
 def test_connect_to_db():
     duckdb_creator = DuckdbClass()
-    conn = duckdb_creator.connect_to_db()
+    conn = duckdb_creator.connect_to_db('output_teste', 'db_name_teste')
     assert conn is not None
