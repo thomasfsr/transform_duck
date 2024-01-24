00cc728457dfa9d4746b6b0ecb07c3a0335727f4
Thomás Freire <thomas.fsr@gmail.com>
2024-01-24 13:15:51 -0300

    atualizando dash

--- tests/test_duckdb.py
+++ tests/test_duckdb.py
@@ -1,28 +1,12 @@
-import os
-import shutil
-import pytest
 from app.db_tools import DuckdbClass
+import os
 
-@pytest.fixture
-def duckdb_creator(request):
-    # Instantiate DuckdbClass
+def test_create_output_directory():
     duckdb_creator = DuckdbClass()
-
-    # Create output directory
-    duckdb_creator.create_output_directory('output_teste')
-
-    # Define a finalizer to clean up the output directory after the test
-    def fin():
-        shutil.rmtree('output_teste')
-
-    # Register the finalizer
-    request.addfinalizer(fin)
-
-    return duckdb_creator
-
-def test_create_output_directory(duckdb_creator):
+    duckdb_creator.create_output_directory('database')
     assert os.path.exists(duckdb_creator.output_dir)
 
-def test_connect_to_db(duckdb_creator):
-    conn = duckdb_creator.connect_to_db('output_teste', 'db_name_teste')
+def test_connect_to_db():
+    duckdb_creator = DuckdbClass()
+    conn = duckdb_creator.connect_to_db('database', 'transactions.db')
     assert conn is not None
