5f674c6cd663564cb2358424fab555fd94870b6a
Thomás Freire <thomas.fsr@gmail.com>
2024-01-26 21:20:49 -0300

    novos testes

--- tests/test_duckdb.py
+++ tests/test_duckdb.py
@@ -1,16 +1,16 @@
 import os
 
-from app.db_tools import DuckdbClass
+from app.db_transform import DuckdbTransform
 
 def test_create_output_directory():
-    duckdb_creator = DuckdbClass(
+    duckdb_creator = DuckdbTransform(
         'database_test', 'tbl_test', 'tests/data_test', 'tests/output_test'
     )
     duckdb_creator.create_output_directory()
     assert os.path.exists(duckdb_creator.output_dir)
 
 def test_connect_to_db():
-    duckdb_creator = DuckdbClass(
+    duckdb_creator = DuckdbTransform(
         'database_test', 'tbl_test', 'tests/data_test', 'tests/output_test'
     )
     conn = duckdb_creator.connect_to_db()
