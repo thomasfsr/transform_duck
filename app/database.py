110fd5799f06e9a207df6ad2128880aa4f235061
Thomás Freire <thomas.fsr@gmail.com>
2024-01-31 11:16:27 -0300

    testes com s3

---
+++ app/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+import pandas as pd
+import psycopg2
+
+load_dotenv()
+
+postgres_conn_str = os.getenv('db_external_url')
+postgres_engine = create_engine(postgres_conn_str)
+
+file = 'db/df_sales_retail.parquet'
+df = pd.read_parquet(file)
+
+df.to_sql('sales', postgres_engine, if_exists='replace', index=False)
+
+postgres_engine.dispose()
